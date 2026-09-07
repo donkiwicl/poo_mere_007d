08a9aaf87cb3727c72771ce6b9cce3fa79315d87
mamkd <ma.mercade@duocuc.cl>
2026-09-07 13:31:35 -0300

    Se agregaron clases.

---
+++ src/main/java/dev/rampmaster/clases_sistema/Estudiante.java
@@ -0,0 +1,9 @@
+package dev.rampmaster.clases_sistema;
+
+public class Estudiante extends Usuario {
+
+    public Estudiante(String nombre, String apellido, String email, String run) {
+        super(nombre, apellido, email, run);
+    }
+
+}
