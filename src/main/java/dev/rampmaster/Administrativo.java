5adf15bc94c9009ec9d94b7a57f4a097f00a7c01
vicente diomedi <vi.diomedi@duocuc.cl>
2026-09-09 13:30:51 -0300

    Se termina de agregar los objetos sala

--- src/main/java/dev/rampmaster/Administrativo.java
+++ src/main/java/dev/rampmaster/Administrativo.java
@@ -1,8 +1,24 @@
 package dev.rampmaster;
 
 public class Administrativo extends Usuario {
-    public Administrativo(String nombre, String apellido, String email, String run) {
-        super(nombre, apellido, email, run);
-    }
+    private String cargo;
+    private int salario;
 
+    public Administrativo(String nombre, String apellido, String email, String run, String cargo, int salario) {
+        super(nombre, apellido, email, run);
+        this.cargo = cargo;
+        this.salario = salario;
+    }
+    public String getCargo() {
+        return cargo;
+    }
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    public int getSalario() {
+        return salario;
+    }
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
 }
--- src/main/java/dev/rampmaster/Sede.java
+++ src/main/java/dev/rampmaster/Sede.java
@@ -3,5 +3,31 @@ package dev.rampmaster;
 import java.util.HashSet;
 
 public class Sede {
-    HashSet<Sala> salas = new HashSet<Sala>();
+    private String nombre_sede;
+    private int cantidad_de_pisos;
+    private HashSet <Piso> piso = new HashSet<Piso>();
+
+    public Sede(String nombre_sede, int cantidad_de_pisos) {
+        this.nombre_sede = nombre_sede;
+        this.cantidad_de_pisos = cantidad_de_pisos;
+    }
+    public String getNombre_sede() {
+        return nombre_sede;
+    }
+    public void setNombre_sede(String nombre_sede) {
+        this.nombre_sede = nombre_sede;
+    }
+    public int getCantidad_de_pisos() {
+        return cantidad_de_pisos;
+    }
+    public void setCantidad_de_pisos(int cantidad_de_pisos) {
+        this.cantidad_de_pisos = cantidad_de_pisos;
+    }
+
+    public HashSet<Piso> getPiso() {
+        return piso;
+    }
+    public void setPiso(HashSet<Piso> piso) {
+        this.piso = piso;
+    }
 }
